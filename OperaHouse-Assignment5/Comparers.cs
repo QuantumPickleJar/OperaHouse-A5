472cb9fac0b2b6b3e79f2403328f9d192af32ac7
kuehnn91 <kuehnn91@UWO8189W0>
2023-05-09 11:17:53 -0500

    Add project files.

---
+++ OperaHouse-Assignment5/Comparers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperaHouse_Assignment5
+{
+    class TicketComparer : IComparer<Event>
+    {
+        public int Compare(Event e1, Event e2)
+        {
+            if (e1.NumAvailableTickets == e2.NumAvailableTickets)
+                return 0;
+            return e1.NumAvailableTickets > e2.NumAvailableTickets ? 1 : 0;
+        }
+    }
+    class TitleComparer : IComparer<Event>
+    {
+        public int Compare(Event e1, Event e2)
+        {
+            return e1.Title.CompareTo(e2.Title);
+        }
+    }
+
+    class PerformerComparer : IComparer<Event>
+    {
+        public int Compare(Event e1, Event e2)
+        {
+            return e1.Performer.Name.CompareTo(e2.Performer.Name);
+        }
+    }
+
+    class PerformerDateComparer : IComparer<Event>
+    {
+        public int Compare(Event e1, Event e2)
+        {
+            if (e1.Performer.Name == e2.Performer.Name)
+                return e1.EventTime.CompareTo(e2.EventTime);
+            else
+            {
+                return e1.Performer.Name.CompareTo(e2.Performer.Name);
+            }
+        }
+    }
+}
+
---
+++ OperaHouse-Assignment5/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperaHouse_Assignment5
+{
+    public class Ticket
+    {
+        public int SeatNumber {get;set;}
+        public double Price { get; set; }
+        public bool Sold { get; set; }
+
+        public Ticket(int seatNumber, double price)
+        {
+            this.SeatNumber = seatNumber;
+            this.Price = price;
+            this.Sold = false;
+        }
+    }
+}
